b03336c4610c04a67b50a9d62f03af39f38db9c2
teplostanski <teplostanski@yandex.ru>
2024-04-02 05:14:54 +0300

    style: add description in to cli help

--- src/commands/index.tsx
+++ src/commands/index.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import {App} from '../components/app.js'
 
 export default class Main extends Command {
+  static description = "Network Checker is a user-friendly CLI (TUI) application that offers real-time IP discovery, ping statistics, and speed testing to optimize your network performance."
   async run() {
     render(<App />)
   }
