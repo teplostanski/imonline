90310d8800a6257072b3682215d1e3a03afb9af7
teplostanski <teplostanski@yandex.ru>
2024-04-14 22:28:12 +0300

    perf: fix path to package.json

--- src/utils/load-package-json.ts
+++ src/utils/load-package-json.ts
@@ -3,6 +3,6 @@ import {dirname, join} from 'node:path'
 import {fileURLToPath} from 'node:url'
 
 export function loadPackageJson() {
-  const pathToPackageJson = join(dirname(fileURLToPath(import.meta.url)), '..', 'package.json')
+  const pathToPackageJson = join(dirname(fileURLToPath(import.meta.url)), '../..', 'package.json')
   return JSON.parse(readFileSync(pathToPackageJson, 'utf8'))
 }
